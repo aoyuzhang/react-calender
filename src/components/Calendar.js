99494a2ffc552bbf2e84a07f35cbf917b90b7101
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-26 14:32:16 -0700

    update

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,6 +3,18 @@ import moment from 'moment'
 
 export default class Calendar extends React.Component{
 
+  state = {
+    dateObject: moment()
+  }
+
+  // firstDayOfMonth = () => {
+  //   let dateObject = this.state.dateObject;
+  //   let firstDay = moment(dateObject)
+  //                  .startOf("month")
+  //                  .format("d");
+  //   return firstDay;
+  // }
+
   weekdayshort = moment.weekdaysShort();
   render(){
     let weekdayshortname =  this.weekdayshort.map(day => {
