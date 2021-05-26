9144b931a1ad7395e12537d5b3db214524f0c080
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-26 14:20:11 -0700

    displying days of the week using moment.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import moment from 'moment'
 
 export default class Calendar extends React.Component{
+
+  weekdayshort = moment.weekdaysShort();
   render(){
+    let weekdayshortname =  this.weekdayshort.map(day => {
+      return(
+        <th key={day} >
+          {day}
+        </th>
+      );
+    })
     return (
       <div>
         <h2>Calendar</h2>
+        <tr>{weekdayshortname}</tr>
       </div>
     );
   }
